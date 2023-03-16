a4c0cf55061b08478cb979aaed5c949d3b45081c
RIDWANE-EL-FILALI <ridwaneelfilali@gmail.com>
2023-03-16 07:50:06 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int	main(void)
 {
-	char tab[100] = "Programming is like building a multilingual puzzle\n";
+	char tab[100] = "\"Programming is like building a multilingual puzzle";
 
 	puts(tab);
 	return (0);
