a450fd8be457f358cdb28dbddb9b4261426c9e08
RIDWANE-EL-FILALI <ridwaneelfilali@gmail.com>
2023-03-16 07:52:13 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include  <stdio.h>
 
+/*
+main entry for the programe
+return 0 for sucess aand 1 for failure
+*/
+
 int	main(void)
 {
 	char tab[100] = "\"Programming is like building a multilingual puzzle";
