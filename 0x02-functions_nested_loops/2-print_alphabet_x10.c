1c0344aaca3c650751e7dfb6d54a42152312b132
RIDWANE-EL-FILALI <ridwaneelfilali@gmail.com>
2023-03-21 03:34:25 -0700

    10 * alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,23 @@
 #include "main.h"
 
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ * Return: x10 a-z
+ */
 void print_alphabet_x10(void)
 {
-	int i;
+	int n, co;
 
-	i = 0;
-	while (i < 10)
+	co = 0;
+
+	while (co < 10)
 	{
-		print_alphabet();
-		i++;
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		co++;
+		_putchar('\n');
 	}
 }
-
