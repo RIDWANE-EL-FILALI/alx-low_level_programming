bc5219dc502ade0a201c77b388c49732c633702f
RIDWANE-EL-FILALI <ridwaneelfilali@gmail.com>
2023-03-21 03:26:28 -0700

    I sometimes suffer from insomnia.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,13 @@
 #include "main.h"
-
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 void print_alphabet(void)
 {
 	char *str = "abcdefghijklmnopqrstuvwxyz";
+
 	while (*str)
 	{
 		_putchar(*str);
