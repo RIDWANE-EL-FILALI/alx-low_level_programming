ece2154c7ac579d40ac937fcc8d5eebbc99b9326
RIDWANE-EL-FILALI <ridwaneelfilali@gmail.com>
2023-03-21 14:18:12 -0700

    0. Multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,9 @@
-# ifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
-void print_remaining_days(int month, int day, int year);
-int largest_number(int a, int b, int c)
 void positive_or_negative(int i);
-#endif
+int largest_number(int, int, int );
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+#endif /*MAIN_H*/
