7e40b5e4a32f218054f3bf3b916205eb92807d8b
RIDWANE-EL-FILALI <ridwaneelfilali@gmail.com>
2023-03-21 03:13:29 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
+#include "main.h"
 /**
 * main - print all base 10 digits
 *
 * Return: 0
 */
 
-void _putchar(char *str)
-{
-	puts(str);
-}
-
 int main(void)
 {
 	_putchar("_putchar");
 	return (0);
+}
+
+void _putchar(char *str)
+{
+        puts(str);
 }
