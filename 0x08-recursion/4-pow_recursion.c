ab3fcd96e69aa0f5bd1dc671dd44fe18c5cf5eac
RIDWANE-EL-FILALI <ridwaneelfilali@gmail.com>
2023-04-05 13:14:27 -0700

    ex 4 in 0x08

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,7 +8,7 @@
  * @y: This is the power number
  *
  * Return: if "y" is bigger than zero raised to the power of "y",
- * return -1 if "y" is lower than zero, return 1 if "y" is equal to 1, 
+ * return -1 if "y" is lower than zero, return 1 if "y" is equal to 1
  */
 int _pow_recursion(int x, int y)
 {
