a3940fa47feca5425234796d33dae4ba14ab9b5c
RIDWANE-EL-FILALI <ridwaneelfilali@gmail.com>
2023-04-17 06:38:14 -0700

    0X0E

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * struct dog - this descripction about which dog
+ * @name: sdfasdf
+ * @age:  asdfas
+ * @owner:  asdf
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
