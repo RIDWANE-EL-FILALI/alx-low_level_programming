9a5ab7f54105d5cb2f6705e266c69283be63e5cf
RIDWANE-EL-FILALI <ridwaneelfilali@gmail.com>
2023-04-27 03:10:39 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,6 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+
+
 size_t print_list(const list_t *h);
 
 
